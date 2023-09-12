4a542470303f5a64f01be1e8a40cdd7657485523
parkseyeonn <yondu0902@gmail.com>
2023-09-12 23:03:52 +0900

    delete & leave server modal

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -4,8 +4,10 @@ import { useEffect, useState } from "react";
 
 import { CreateChannelModal } from "../modals/create-channel-modal";
 import { CreateServerModal } from "../modals/create-server-modal";
+import { DeleteServerModal } from "../modals/delete-server-modal";
 import { EditServerModal } from "../modals/edit-server-modal";
 import { InviteModal } from "../modals/invite-modal";
+import { LeaveServerModal } from "../modals/leave-server-modal";
 import { MembersModal } from "../modals/members-modal";
 
 export const ModalProvider = () => {
@@ -22,8 +24,10 @@ export const ModalProvider = () => {
     <>
       <CreateChannelModal />
       <CreateServerModal />
+      <DeleteServerModal />
       <EditServerModal />
       <InviteModal />
+      <LeaveServerModal />
       <MembersModal />
     </>
   );
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -102,13 +102,19 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         ) : null}
         {isModerator ? <DropdownMenuSeparator /> : null}
         {isAdmin ? (
-          <DropdownMenuItem className='text-rose-500 px-3 py-2 text-sm cursor-pointer'>
+          <DropdownMenuItem
+            onClick={() => onOpen("deleteServer", { server })}
+            className='text-rose-500 px-3 py-2 text-sm cursor-pointer'
+          >
             Delete Server
             <Trash className='h-4 w-4 ml-auto' />
           </DropdownMenuItem>
         ) : null}
         {!isAdmin ? (
-          <DropdownMenuItem className='text-rose-500 px-3 py-2 text-sm cursor-pointer'>
+          <DropdownMenuItem
+            onClick={() => onOpen("leaveServer", { server })}
+            className='text-rose-500 px-3 py-2 text-sm cursor-pointer'
+          >
             Leave Server
             <LogOut className='h-4 w-4 ml-auto' />
           </DropdownMenuItem>
